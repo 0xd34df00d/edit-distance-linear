645116a5511791818241bace90e5962065b8c1d5
0xd34df00d <0xd34df00d@gmail.com>
2019-11-30 18:23:24 -0500

    Initial implementation with STUArray

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,12 @@
 module Main where
 
+import qualified Data.ByteString.Char8 as BS
+import Text.EditDistance.Linear
+
 main :: IO ()
-main = undefined
+main = do
+  let s1 = BS.replicate 10000 'a'
+  let s2 = s1
+  let s3 = BS.replicate 10000 'b'
+  print $ levenshteinDistance s1 s2
+  print $ levenshteinDistance s1 s3
--- src/Text/EditDistance/Linear.hs
+++ src/Text/EditDistance/Linear.hs
@@ -1,1 +1,34 @@
+{-# LANGUAGE BangPatterns #-}
+
 module Text.EditDistance.Linear where
+
+import qualified Data.Array.Base as A(unsafeRead, unsafeWrite)
+import qualified Data.Array.ST as A
+import qualified Data.ByteString.Char8 as BS
+import Control.Monad
+import Control.Monad.ST
+
+levenshteinDistance :: BS.ByteString -> BS.ByteString -> Int
+levenshteinDistance s1 s2 = runST $ do
+  v0Init <- A.newListArray (0, n) [0..]
+  v1Init <- A.newArray_ (0, n)
+  forM_ [0 .. m - 1] $ \i -> do
+    let (v0, v1) | even i = (v0Init, v1Init)
+                 | otherwise = (v1Init, v0Init)
+    loop i v0 v1
+  A.unsafeRead (if even m then v0Init else v1Init) n
+
+  where
+    m = BS.length s1
+    n = BS.length s2
+
+    loop :: Int -> A.STUArray s Int Int -> A.STUArray s Int Int -> ST s ()
+    loop i v0 v1 = do
+      A.unsafeWrite v1 0 (i + 1)
+      let !s1char = s1 `BS.index` i
+      forM_ [0..n - 1] $ \(!j) -> do
+        delCost <- v0 `A.unsafeRead` (j + 1)
+        insCost <- v1 `A.unsafeRead` j
+        substCostBase <- v0 `A.unsafeRead` j
+        let !substCost = if s1char == s2 `BS.index` j then 0 else 1
+        A.unsafeWrite v1 (j + 1) $ min (substCost + substCostBase) $ 1 + min delCost insCost
