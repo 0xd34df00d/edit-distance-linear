61094cd20107a970ea15dfeb7e5c8951e1b6e9b1
0xd34df00d <0xd34df00d@gmail.com>
2019-11-30 18:50:11 -0500

    Use unsafe string indexing

--- src/Text/EditDistance/Linear.hs
+++ src/Text/EditDistance/Linear.hs
@@ -6,6 +6,7 @@ module Text.EditDistance.Linear where
 import qualified Data.Array.Base as A(unsafeRead, unsafeWrite)
 import qualified Data.Array.ST as A
 import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Unsafe as BU
 import Control.Monad
 import Control.Monad.ST
 
@@ -24,13 +25,13 @@ levenshteinDistance s1 s2 = runST $ do
     loop !i !v0 !v1 | i == m = pure ()
                     | otherwise = do
       A.unsafeWrite v1 0 (i + 1)
-      let !s1char = s1 `BS.index` i
+      let !s1char = s1 `BU.unsafeIndex` i
       let go !j | j == n = pure ()
                 | otherwise = do
             delCost <- v0 `A.unsafeRead` (j + 1)
             insCost <- v1 `A.unsafeRead` j
             substCostBase <- v0 `A.unsafeRead` j
-            let !substCost = if s1char == s2 `BS.index` j then 0 else 1
+            let !substCost = if s1char == s2 `BU.unsafeIndex` j then 0 else 1
             A.unsafeWrite v1 (j + 1) $ min (substCost + substCostBase) $ 1 + min delCost insCost
             go (j + 1)
       go 0
