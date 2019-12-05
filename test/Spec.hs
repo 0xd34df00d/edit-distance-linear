1077b0f336c347a415e30a379672644b11a1befd
0xd34df00d <0xd34df00d@gmail.com>
2019-12-05 18:31:11 -0500

    Check that behaviour matches that of edit-distance

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,10 +1,14 @@
 {-# LANGUAGE ViewPatterns #-}
 
 import qualified Data.ByteString.Char8 as BS
+import qualified Text.EditDistance as TED
 import Test.Hspec
 import Test.QuickCheck
 
 import Text.EditDistance.Linear
+
+baseLevDist :: String -> String -> Int
+baseLevDist = TED.levenshteinDistance TED.defaultEditCosts
 
 main :: IO ()
 main = hspec $ do
@@ -15,3 +19,6 @@ main = hspec $ do
       property $ \(BS.pack . getNonEmpty -> s) -> levenshteinDistance s (BS.map succ s) > 0
     it "Edit distance to completely different string is not greater than string's length" $ do
       property $ \(BS.pack -> s) -> levenshteinDistance s (BS.map succ s) <= BS.length s
+  describe "Comparing to edit-distance" $
+    it "Edit distances always match" $
+      property $ \(getASCIIString -> s1) (getASCIIString -> s2) -> levenshteinDistance (BS.pack s1) (BS.pack s2) `shouldBe` baseLevDist s1 s2
