64cf0ef0c38734103f3c50e37309d7b864784b66
0xd34df00d <0xd34df00d@gmail.com>
2019-12-05 18:25:19 -0500

    Throw up a few tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,17 @@
+{-# LANGUAGE ViewPatterns #-}
+
+import qualified Data.ByteString.Char8 as BS
+import Test.Hspec
+import Test.QuickCheck
+
+import Text.EditDistance.Linear
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Obvious cases" $ do
+    it "Edit distance to self is always 0" $
+      property $ \(BS.pack -> s) -> levenshteinDistance s s `shouldBe` 0
+    it "Edit distance to completely different string is strictly positive" $ do
+      property $ \(BS.pack . getNonEmpty -> s) -> levenshteinDistance s (BS.map succ s) > 0
+    it "Edit distance to completely different string is not greater than string's length" $ do
+      property $ \(BS.pack -> s) -> levenshteinDistance s (BS.map succ s) <= BS.length s
