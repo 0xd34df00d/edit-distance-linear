e1f806a444a0d1d5297479e71e7761f1c3a09be9
0xd34df00d <0xd34df00d@gmail.com>
2019-12-05 18:50:29 -0500

    Throw up stub benchmark

---
+++ bench/Bench.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE BangPatterns, OverloadedStrings #-}
+
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.Text as T
+import Criterion.Main
+import Data.Text.Metrics
+
+import Text.EditDistance.Linear
+
+main :: IO ()
+main = defaultMain
+  [ bgroup "same string/ed-linear"      [ bench (show num) $ nf (\s -> levenshteinDistance s s) $ BS.replicate num 'a'
+                                        | num <- [ 1000, 2000, 5000, 10000, 20000 ]
+                                        ]
+  , bgroup "same string/text-metrics"   [ bench (show num) $ nf (uncurry levenshtein) (T.replicate num "a", T.replicate num "a")
+                                        | num <- [ 1000, 2000, 5000, 10000, 20000 ]
+                                        ]
+  , bgroup "diff strings/ed-linear"     [ bench (show num) $ nf (uncurry levenshteinDistance) (BS.replicate num 'a', BS.replicate num 'b')
+                                        | num <- [ 1000, 2000, 5000, 10000, 20000 ]
+                                        ]
+  , bgroup "diff strings/text-metrics"  [ bench (show num) $ nf (uncurry levenshtein) (T.replicate num "a", T.replicate num "b")
+                                        | num <- [ 1000, 2000, 5000, 10000, 20000 ]
+                                        ]
+  ]
