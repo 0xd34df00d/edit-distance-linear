36ef5cdc1d22c72368b50911439e3b21ae251c72
0xd34df00d <0xd34df00d@gmail.com>
2019-12-05 19:08:05 -0500

    Avoid string sizes duplication in the benchmarks

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -10,15 +10,17 @@ import Text.EditDistance.Linear
 main :: IO ()
 main = defaultMain
   [ bgroup "same string/ed-linear"      [ bench (show num) $ nf (\s -> levenshteinDistance s s) $ BS.replicate num 'a'
-                                        | num <- [ 1000, 2000, 5000, 10000, 20000 ]
+                                        | num <- nums
                                         ]
   , bgroup "same string/text-metrics"   [ bench (show num) $ nf (uncurry levenshtein) (T.replicate num "a", T.replicate num "a")
-                                        | num <- [ 1000, 2000, 5000, 10000, 20000 ]
+                                        | num <- nums
                                         ]
   , bgroup "diff strings/ed-linear"     [ bench (show num) $ nf (uncurry levenshteinDistance) (BS.replicate num 'a', BS.replicate num 'b')
-                                        | num <- [ 1000, 2000, 5000, 10000, 20000 ]
+                                        | num <- nums
                                         ]
   , bgroup "diff strings/text-metrics"  [ bench (show num) $ nf (uncurry levenshtein) (T.replicate num "a", T.replicate num "b")
-                                        | num <- [ 1000, 2000, 5000, 10000, 20000 ]
+                                        | num <- nums
                                         ]
   ]
+  where
+    nums = [ 1000, 2000, 5000, 10000, 20000 ]
